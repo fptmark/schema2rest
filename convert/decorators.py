6fb0f397de6a0e556fb2d9e9bc7610989f115e4b
Mark Malamut <mark40353@gmail.com>
2025-03-28 11:15:48 -0700

    cleanup decorator handling

--- convert/decorators.py
+++ convert/decorators.py
@@ -15,19 +15,20 @@ ABSTRACT = "@abstract"
 INCLUDES = "@include"
 SERVICE = "@service"
 UI = "@ui"
-LABELS = "@labels"
 OPERATION = "@operations"
+
 OPERATIONS = ["create", "read", "update", "delete"]
 
 # All supported decorators
-FIELD_DECORATORS = [UNIQUE, VALIDATE, UI]
-ENTITY_DECORATORS = [SERVICE, UNIQUE, OPERATION, UI, ABSTRACT, INCLUDES, LABELS ]
+COMMON_DECORATORS = [UI, UNIQUE]
+FIELD_DECORATORS = COMMON_DECORATORS + [VALIDATE]
+ENTITY_DECORATORS = COMMON_DECORATORS + [SERVICE, OPERATION, ABSTRACT, INCLUDES]
 ALL_DECORATORS = FIELD_DECORATORS + ENTITY_DECORATORS + [DICTIONARY]
 
 
 # Constants 
 FIELDS = 'fields'
-METADATA = 'ui_metadata'
+UI_METADATA = 'ui'
 class Decorator:
     """
     Class to handle all decorator processing
@@ -93,26 +94,31 @@ class Decorator:
         # Process based on decorator type
         if decorator == DICTIONARY and text.startswith("%%"):
             self._process_dictionary(decoration)
-
         elif entity_name:
-            decorator_text = decorator_text[len(decorator):].strip()  # remove the decorator from the text
-            if decorator in FIELD_DECORATORS and field_name:
-                self._process_field_entity_decorations(decorator, entity_name, field_name, decorator_text)
+            if decorator in COMMON_DECORATORS and not field_name:  # check if we have an field decorator
+                decoration, field_name = self._get_field_name(decoration, field_name)
+            if field_name and decorator in FIELD_DECORATORS:
+                self._process_field_entity_decorations(decorator, entity_name, field_name, decoration)
+            # Handle UI and UNIQUE decorators at the entity level
             elif decorator in ENTITY_DECORATORS:
+                # Handle UI and UNIQUE at the entity level
                 self._process_entity_decorations(decorator, entity_name, decoration)
 
-    
+
     def _process_field_entity_decorations(self, decorator: str, entity_name: str, field_name: Optional[str], text: str):
         # if there are multiple decorators, split and recurse
         index = text.find('@')
         if index > -1:
-            next_text = text[index:].strip()    # text for next decorator
-            words = next_text.split(' ')        # get decorator name
-            text = text[:index].strip()         # text for current decorator
-            next_text = next_text[len(words[0]):].strip()  # remove the decorator from the text
-            self._process_field_entity_decorations(words[0], entity_name, field_name, next_text)
-        if decorator in [VALIDATE, UI]:
-            # text = text[len(decorator):].strip()
+            # process current decorator and then recurse
+            current_text = text[:index].strip()         # text for current decorator
+            self._process_field_entity_decorations(decorator, entity_name, field_name, current_text)
+            
+            # process next decorator
+            text = text[index:].strip()         # text for next decorator
+            decorator = text.split(' ')[0]      # get decorator name
+            text = text[len(decorator):].strip()     # remove the decorator from the text
+            self._process_field_entity_decorations(decorator, entity_name, field_name, text)
+        elif decorator in [VALIDATE, UI]:
             self._add_field_data(decorator, entity_name, field_name, text)
         elif decorator == UNIQUE:
             self._add_unique(entity_name, field_name)
@@ -129,32 +135,16 @@ class Decorator:
         """
         
         # Update entity based on decoration type
-        if decorator in [ ABSTRACT, INCLUDES, SERVICE, LABELS]:
+        if decorator == UI:       # may be an entity or field decorator
+            words = text.split(' ')
+            if words[0] != '{':     # assume the word is an field name so process as a field decorator
+                self._add_field_data(decorator, entity_name, words[0], ' '.join(words[1:]))
+                return
+
+        if decorator in [ ABSTRACT, INCLUDES, SERVICE, UI]:
             self._add_entity_decoration(decorator, entity_name, text)
         elif decorator == UNIQUE:
             self._add_unique(entity_name, text)
-        elif decorator == UI:
-            # handle all 5 forms of UI decorator at entity level - <entity>.*, <entity>.<field>, id, <field>, *
-            words = text.split(' ')
-            if words[0] == '{' or words[1] != '{': # This should always be false
-                print(f'*** Error: UI entity decorator for {entity_name} must include fields: {text}')
-                sys.exit(-1)
-            entity_defn = words[0].split('.')
-            # Process id field
-            if words[0] == 'id':
-                field = "_id"
-                fields = [field]
-            # check for named/inherited entity - if not it is the current entity
-            elif len(entity_defn) > 1:
-                fields = self._get_fields(entity_defn[0])
-                field = entity_defn[1]
-            else:
-                fields = self._get_fields(entity_name)  # use all fields for the current entity
-                field = words[0]
-
-            text = text[text.index('{'):]
-            for field in fields:
-                self._process_field_entity_decorations(UI, entity_name, field, text)
 
         elif decorator == OPERATION:
             operation = ''
@@ -167,16 +157,25 @@ class Decorator:
                 self._add_entity_decoration(decorator, entity_name, operation)
 
 
-    def _get_fields(self, entity_name: str) -> List[str]:
-        entity = self.entities.get(entity_name)
-        fields = []
-        for field, _ in entity.get(FIELDS, {}).items():
-            fields.append(field)
-        return fields
+    # def _get_fields(self, entity_name: str) -> List[str]:
+    #     entity = self.entities.get(entity_name)
+    #     fields = []
+    #     for field, _ in entity.get(FIELDS, {}).items():
+    #         fields.append(field)
+    #     return fields
+
+
+    def _get_field_name(self, decoration, field_name):
+        words = decoration.split(' ')
+        if words[0] != '{':
+            field_name = words[0]
+            decoration = ' '.join(words[1:]).strip()  # remove the decorator from the text
+        return decoration, field_name
 
 
     # Handles @validate and @ui
     def _add_field_data(self, decorator, entity_name, field_name, text):
+        field_name = "_id" if field_name.lower() == "id" else field_name
         # remove trailing comma for mutliple decorators on a line
         if text.endswith(','):
             text = text[:-1]
@@ -194,7 +193,7 @@ class Decorator:
 
                 # UI Metadata goes in a subsection
                 if decorator == UI:
-                    field.setdefault(METADATA, {}).update(data)
+                    field.setdefault(UI_METADATA, {}).update(data)
                 else:
                     field.update(data)
 
@@ -209,9 +208,6 @@ class Decorator:
         entity = self.entities[entity_name] 
         if decorator == ABSTRACT:
             entity['abstract'] = True
-        elif decorator == LABELS:
-            value = json5.loads(value)
-            entity.setdefault('labels', {}).update(value)
         elif decorator == INCLUDES:
             # add a copy of the abstraction fields to the current entity.  set the displayAfterField so they all appear after the core entity fields
             abstraction = self.entities.get(value)
@@ -223,13 +219,16 @@ class Decorator:
                 # prior_field = entity_names[-1]
                 prior_field = -1    # special naming for included entity fields
                 for field_name, field_value in fields_copy.items():
-                    field_value.setdefault(METADATA, {}).update({'displayAfterField': str(prior_field)})
+                    field_value.setdefault(UI_METADATA, {}).update({'displayAfterField': str(prior_field)})
                     prior_field = prior_field - 1
 
                 entity.setdefault(FIELDS, []).update(fields_copy)
             else:
                 print(f'*** Error: abstraction fields for {value} not found')
                 sys.exit(-1)
+        elif decorator == UI:
+            data = json5.loads(value)
+            entity.setdefault(UI_METADATA, {}).update(data)
         else:
             entity.setdefault(decorator[1:], []).append(value.strip())
 
