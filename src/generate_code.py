bc37c1ee25ddef95c422fde42114024c7e7762e9
Mark Malamut <mark40353@gmail.com>
2025-06-10 23:44:56 -0700

    no more dynamic db generation - using driver model

--- src/generate_code.py
+++ src/generate_code.py
@@ -9,12 +9,12 @@ from pathlib import Path
 from generators.models.gen_model_main import generate_models
 from generators.gen_routes import generate_routes
 from generators.gen_service_routes import generate_service_routes
-from generators.gen_db import generate_db
+# from generators.gen_db import generate_db
 from generators.gen_main import generate_main
 from convert.schemaConvert import convert_schema
 from tools.mongo.update_indicies import update_indexes
 
-def generate_code(schema_file, base_output_dir, project_name, config_file=None):
+def generate_code(schema_file, generic_file_dir, base_output_dir, project_name, config_file=None):
     """
     Main entry point for code generation
     """
@@ -26,7 +26,7 @@ def generate_code(schema_file, base_output_dir, project_name, config_file=None):
             generate_routes(yaml, base_output_dir)
             # generate_db(yaml, base_output_dir)
             generate_models(yaml, base_output_dir)
-            generate_service_routes(yaml, base_output_dir)
+            generate_service_routes(yaml, generic_file_dir, base_output_dir)
             if config_file:
                 update_indexes(yaml, config_file)
             else:
@@ -46,11 +46,11 @@ def generate_code(schema_file, base_output_dir, project_name, config_file=None):
     return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python generate_code.py <schema.mmd> <base_output_path> <project name> [<config_file>]")
+    if len(sys.argv) < 5:
+        print("Usage: python generate_code.py <schema.mmd> <generic_file_dir> <base_output_path> <project name> [<config_file>]")
         sys.exit(1)
 
-    optional_param_start = 4
+    optional_param_start = 5
 
     config_file = None if len(sys.argv) <= optional_param_start + 1 else sys.argv[optional_param_start + 1]
-    results = generate_code(sys.argv[1], sys.argv[2], sys.argv[3], config_file)
+    results = generate_code(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], config_file)
